b69a549041d3eab97506d3e88c15a191135c9282
Gennadii Saltyshchak <GennadyGS@gmail.com>
2016-12-24 12:59:21 +0200

    Add Weather.Loader project

---
+++ Weather.Loader/Loader.fs
@@ -0,0 +1,28 @@
+﻿module Weather.Loader.Loader
+
+open FSharp.Data.Sql
+
+let [<Literal>] SchemaConnectionString =
+    "Data Source=gennadygs.database.windows.net;Initial Catalog=Weather;Integrated Security=False;User ID=gennadygs;Password=zl0zYH`};Connect Timeout=60;Encrypt=False;TrustServerCertificate=True;ApplicationIntent=ReadWrite;MultiSubnetFailover=False"
+
+type SqlProvider = SqlDataProvider<ConnectionString = SchemaConnectionString>
+
+let dataContext = SqlProvider.GetDataContext()
+
+let observationsTable = dataContext.Dbo.Observations
+
+let insertRow () =
+    let now = System.DateTime.Now
+    let row = observationsTable.Create()
+    row.StationNumber <- 33345
+    row.Date <- now
+    row.Hour <- byte (now.Hour)
+    dataContext.SubmitUpdates()
+
+let getObservations () = 
+    query {
+        for o in observationsTable do
+        select (o.Date, o.Hour, o.StationNumber) 
+    } 
+    |> Seq.toList
+
---
+++ Weather.Loader/Scripts/load-references-debug.fsx
@@ -0,0 +1,6 @@
+// Warning: generated file; your changes could be lost when a new file is generated.
+#I __SOURCE_DIRECTORY__
+#r "System.Core.dll"
+#r "System.dll"
+#r "System.Numerics.dll"
+#r "../../packages/SQLProvider.1.0.40/lib/FSharp.Data.SqlProvider.dll"
