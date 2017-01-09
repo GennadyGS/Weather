696350b9cc5dccd24db68c84c0912b03f622d631
Gennadii Saltyshchak <GennadyGS@gmail.com>
2017-01-09 14:40:03 +0200

    Add Loader module

--- Weather.Composition/Loader.fs
+++ Weather.Composition/Loader.fs
@@ -1,5 +1,25 @@
 ﻿module Weather.Loader
 
 open FSharp.Data
+open System
+open Weather.Model
 
-Http.RequestString("http://www.ogimet.com/cgi-bin/getsynop?block=33345&begin=201701010000&end=201701312359")
+let private splitString (char : char) (string : string) : string[] = 
+    string.Split([|char|])
+
+let private formatDate (date : DateTime) : string = 
+    date.ToString("yyyyMMddHHmm")
+
+let private getRequestUrl (stationNumber : int) (dateFrom : DateTime) (dateTo : DateTime) = 
+    sprintf "http://www.ogimet.com/cgi-bin/getsynop?block=%d&begin=%s&end=%s" stationNumber (dateFrom |> formatDate) (dateTo |> formatDate)
+
+
+let private parseObservation (string : string) : Observation = 
+    raise (NotImplementedException ())
+
+let loadObservations (stationNumber : int) (dateFrom : DateTime) (dateTo : DateTime) : Observation list = 
+    getRequestUrl stationNumber dateFrom dateTo
+        |> Http.RequestString 
+        |> splitString '\n' 
+        |> List.ofArray 
+        |> List.map parseObservation
