6e9dbd95c1d745295fa6aa8cc20318b722d20905
Gennadii Saltyshchak <GennadyGS@gmail.com>
2017-01-20 17:40:17 +0200

    Add tests project

---
+++ Weather.Tests/LoaderTests.fs
@@ -0,0 +1,7 @@
+﻿module Weather.Tests.LoaderTests
+
+open Xunit
+    
+[<Fact>]
+let ``Test loading``() = 
+    Weather.Loader.Loader.saveObservations([|{Time = {Date = System.DateTime.Now; Hour = 12uy}; StationNumber = 0; Temperature = -1.3m}|])
