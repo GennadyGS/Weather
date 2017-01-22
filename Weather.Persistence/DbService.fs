68687d3bbe05c0e6a3c609b696df71794acb724a
Gennadii Saltyshchak <GennadyGS@gmail.com>
2017-01-22 22:20:38 +0200

    getLastObservationTime fix

--- Weather.Persistence/DbService.fs
+++ Weather.Persistence/DbService.fs
@@ -47,13 +47,13 @@ let getObservations () =
 let getLastObservationTime (stationNumber : string) (interval : DateTimeInterval) : DateTime option = 
     let dataContext = SqlProvider.GetDataContext()
     let observationsTable = dataContext.Dbo.Observations
+    let observationDatesQuery = query {
+        for o in observationsTable do
+        select (o.StationNumber, o.Date.AddHours(float(o.Hour)))
+    }
     query {
-        let observationDates = query {
-            for o in observationsTable do
-            where (o.StationNumber = stationNumber)
-            select (o.Date.AddHours(float(o.Hour)))
-        }
-        for od in observationDates do
-        where (od >= interval.From && od <= interval.To)
-        maxBy (Some (od))
+        for od in observationDatesQuery do
+        where (fst(od) = stationNumber)
+        where (snd(od) >= interval.From && snd(od) <= interval.To)
+        maxBy (Some (snd(od)))
     }
