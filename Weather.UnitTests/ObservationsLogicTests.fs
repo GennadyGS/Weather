fbb9764a51aeda4192fa5cda1fcdce148eca93d9
Gennadii Saltyshchak <GennadyGS@gmail.com>
2017-02-18 13:07:09 +0200

    Add unit tests

--- Weather.UnitTests/ObservationsLogicTests.fs
+++ Weather.UnitTests/ObservationsLogicTests.fs
@@ -1,8 +1,11 @@
 ﻿module Weather.UnitTests.ObservationsLogicTests
 
+open FsCheck
 open FsCheck.Xunit
 open Swensen.Unquote
 open Weather.Logic
+open System
+open Weather.Utils
 
 [<Property>]
 let ``FillNewData calls getMaxObservationTime`` 
@@ -28,3 +31,65 @@ let ``FillNewData calls getMaxObservationTime``
 
     // Assert
     getLastObservationTimeCalled =! true
+
+[<Property>]
+let ``FillNewData does not call fetchObservations when last observation time plus 1 munute is greater then interval.To`` 
+        maxObservationTime
+        observations 
+        saveObservations 
+        stationNumber 
+        interval = 
+    
+    // Restrictions
+    (maxObservationTime > interval.To.AddMinutes(1.0)) ==> lazy
+    
+    // Arrange
+    let getLastObservationTime _ _ = 
+        Some maxObservationTime
+
+    let mutable fetchObservationsCalled = false
+    let fetchObservations _ _ =
+        fetchObservationsCalled <- true
+        observations
+
+    // Act
+    Observations.fillNewData 
+        getLastObservationTime 
+        fetchObservations
+        saveObservations 
+        stationNumber
+        interval
+
+    // Assert
+    fetchObservationsCalled =! false
+
+[<Property>]
+let ``FillNewData calls fetchObservations when last observation time plus 1 munute is lower then interval.To`` 
+        maxObservationTime
+        observations 
+        saveObservations 
+        stationNumber 
+        interval = 
+    
+    // Restrictions
+    (maxObservationTime < interval.To.AddMinutes(1.0)) ==> lazy
+    
+    // Arrange
+    let getLastObservationTime _ _ = 
+        Some maxObservationTime
+
+    let mutable fetchObservationsCalled = false
+    let fetchObservations _ _ =
+        fetchObservationsCalled <- true
+        observations
+
+    // Act
+    Observations.fillNewData 
+        getLastObservationTime 
+        fetchObservations
+        saveObservations 
+        stationNumber
+        interval
+
+    // Assert
+    fetchObservationsCalled =! true
