ddf1f5123d0715d7e781409c305d75f4159f922a
Gennadii Saltyshchak <GennadyGS@gmail.com>
2017-04-01 13:43:35 +0300

    Remove function CompositionRoot.fillNewData

--- Weather.Composition/CompositionRoot.fs
+++ Weather.Composition/CompositionRoot.fs
@@ -5,13 +5,6 @@ open Weather.Persistence
 open Weather.DataProvider
 open Weather.Logic
 
-let fillNewData connectionString minTimeSpan stationNumber interval =
-    DbService.getLastObservationTime connectionString stationNumber interval
-        |> Tuple.mapSecondOption (Weather.Logic.Observations.getMissingInterval minTimeSpan interval)
-        |> Option.map ObservationsProvider.fetchObservationsByInterval
-        |> Option.map (DbService.insertParseObservationsResultList connectionString)
-        |> ignore
-
 let fillNewDataForStationList connectionString minTimeSpan stationList interval =
     DbService.getLastObservationTimeList connectionString stationList interval
         |> List.choose (Tuple.mapSecondOption (Weather.Logic.Observations.getMissingInterval minTimeSpan interval))
--- Weather.IntegrationTests/CompositionRootTests.fs
+++ Weather.IntegrationTests/CompositionRootTests.fs
@@ -10,15 +10,6 @@ type CompositionRootTests() =
     inherit DbTests()
 
     [<Fact>]
-    let ``FillNewData for the last day do not throw exception``() =
-        Weather.Composition.CompositionRoot.fillNewData 
-            Settings.ConnectionStrings.Weather 
-            Settings.MinTimeSpan
-            33345 
-            { From = DateTime.UtcNow.AddDays(-1.0)
-              To = DateTime.UtcNow }
-
-    [<Fact>]
     let ``FillNewDataForStationList for emply list should not save observations``() =
         Weather.Composition.CompositionRoot.fillNewDataForStationList
             Settings.ConnectionStrings.Weather 
