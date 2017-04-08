09e5c66666aa5c7807b6ad15a232eb2d6a9f5335
Gennadii Saltyshchak <GennadyGS@gmail.com>
2017-04-08 11:23:13 +0300

    Refactoring: move partitionResult function to logic

--- Weather.Composition/CompositionRoot.fs
+++ Weather.Composition/CompositionRoot.fs
@@ -8,6 +8,6 @@ open Weather.Logic
 let fillNewDataForStations connectionString minTimeSpan interval stationList =
     DbService.getLastObservationTimesForStations connectionString interval stationList
         |> List.choose (Tuple.mapSecondOption (Weather.Logic.Observations.getMissingInterval minTimeSpan interval))
-        |> List.map (ObservationsProvider.fetchObservationsByInterval)
-        |> List.map (DbService.insertParseObservationsResultList connectionString)
-        |> ignore
+        |> List.collect ObservationsProvider.fetchObservationsByInterval
+        |> Weather.Logic.Observations.partitionResults 
+        |> DbService.insertParseObservationsResultList connectionString
