b0de9d0fddb32ec288ac90fd4964a908937d3bf4
Gennadii Saltyshchak <GennadyGS@gmail.com>
2017-04-15 15:32:30 +0300

    Cleanup code of Results module

--- Weather.Utils/Result.fs
+++ Weather.Utils/Result.fs
@@ -1,43 +1,36 @@
 ﻿module Weather.Utils.Result
 
-let map f xResult = 
-    match xResult with
-    | Success success -> Success (f success)
+let map func = function
+    | Success success -> Success (func success)
     | Failure failure -> Failure failure
 
 let mapBoth mapSuccess mapFailure = function
     | Success success -> mapSuccess success
     | Failure failure -> mapFailure failure
 
-let bind f xResult = 
-    match xResult with
-    | Success success -> f success
+let bind func = function
+    | Success success -> func success
     | Failure failure -> Failure failure
 
-let mapToList func result = 
-    match result with
-        | Success success -> func success |> List.map Success
-        | Failure failure -> [Failure failure]
+let mapToList func = function
+    | Success success -> func success |> List.map Success
+    | Failure failure -> [Failure failure]
 
-let bindToList func result = 
-    match result with
-        | Success success -> func success
-        | Failure failure -> [Failure failure]
+let bindToList func = function
+    | Success success -> func success
+    | Failure failure -> [Failure failure]
 
-let mapToOption func result = 
-    match result with
-        | Success success -> func success |> Option.map Success
-        | Failure failure -> Some (Failure failure)
+let mapToOption func = function
+    | Success success -> func success |> Option.map Success
+    | Failure failure -> Some (Failure failure)
 
-let mapFailureToOption func result = 
-    match result with
-        | Success success -> Some (Success success)
-        | Failure failure -> func failure |> Option.map Failure
+let mapFailureToOption func = function
+    | Success success -> Some (Success success)
+    | Failure failure -> func failure |> Option.map Failure
 
-let bindToOption func result = 
-    match result with
-        | Success success -> func success
-        | Failure failure -> Some (Failure failure)
+let bindToOption func = function
+    | Success success -> func success
+    | Failure failure -> Some (Failure failure)
 
 type ResultBuilder() =
     member this.Return x = Success x
