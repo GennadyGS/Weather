55d051487c34d3592a39bda2de35ae9f40abc8bb
Gennadii Saltyshchak <GennadyGS@gmail.com>
2017-04-17 17:03:19 +0300

    Add logging to integration tests project

--- Weather.IntegrationTests/CompositionRootTests.fs
+++ Weather.IntegrationTests/CompositionRootTests.fs
@@ -10,9 +10,10 @@ open Weather.Composition
 
 type CompositionRootTests() =
     inherit DbTests()
+    interface IClassFixture<LoggingTestFixture>
 
     [<Fact>]
-    let ``FillNewDataForStations for emply list should not save observations``() =
+    member this.``FillNewDataForStations for emply list should not save observations``() =
         let results = 
             CompositionRoot.fillNewDataForStations
                 Settings.ConnectionStrings.Weather 
@@ -25,9 +26,9 @@ type CompositionRootTests() =
         DbService.getObservations Settings.ConnectionStrings.Weather () =! []
 
     [<Fact>]
-    let ``FillNewDataForStations for the last day do not throw exception``() =
+    member this.``FillNewDataForStations for the last day do not throw exception``() =
         let results = 
-            Weather.Composition.CompositionRoot.fillNewDataForStations
+            CompositionRoot.fillNewDataForStations
                 Settings.ConnectionStrings.Weather 
                 Settings.MinTimeSpan
                 { From = DateTime.UtcNow.AddDays(-1.0)
---
+++ Weather.IntegrationTests/LoggingTestFixture.fs
@@ -0,0 +1,12 @@
+﻿namespace Weather.IntegrationTests
+
+open System
+open log4net
+
+type LoggingTestFixture() =
+    do
+        log4net.Config.XmlConfigurator.Configure() |> ignore
+
+    interface IDisposable with
+        member __.Dispose() = 
+            ()        
