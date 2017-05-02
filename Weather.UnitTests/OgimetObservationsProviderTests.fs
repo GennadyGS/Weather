efd15e6ce5ea2c9409312f011c136b6fa6ba7fdd
Gennadii Saltyshchak <GennadyGS@gmail.com>
2017-05-02 13:25:09 +0300

    Add unit test for OgimetObservationsProvider

--- Weather.UnitTests/OgimetObservationsProviderTests.fs
+++ Weather.UnitTests/OgimetObservationsProviderTests.fs
@@ -9,6 +9,7 @@ open Weather.Synop
 open Weather.Model
 open Weather.DataProvider
 open System.Net
+open Weather.Utils.Result
 
 type OgimetObservationsProviderTests() =
     inherit GeneratorTests()
@@ -22,19 +23,21 @@ type OgimetObservationsProviderTests() =
             (synopStr : SingleLineString) =
 
         let roundedDate = DateTime.roundToHours date
-        let synopParser _ =
-            Success { StationNumber = stationNumber.Get
-                      Temperature = temperature }
-
         let observationString = 
             sprintf "%05d,%04d,%02d,%02d,%02d,%02d,AAXX %02d%02d1 %s" 
                 stationNumber.Get date.Year date.Month date.Day date.Hour date.Minute 
                 roundedDate.Day roundedDate.Hour synopStr.Get
 
+        let synopParser _ =
+            Success { StationNumber = stationNumber.Get
+                      Temperature = temperature }
         let httpGetFunc _ _ = 
             (HttpStatusCode.OK, observationString)
 
-        let expectedResult = 
+        let result = 
+            OgimetObservationsProvider.fetchObservationsByInterval 
+                synopParser httpGetFunc (StationNumber stationNumber.Get, interval)        
+        result =! 
             [Success 
                 { Header = 
                     { StationNumber = StationNumber stationNumber.Get
@@ -43,7 +46,25 @@ type OgimetObservationsProviderTests() =
                           Hour = byte roundedDate.Hour }}
                   Temperature = temperature}]
 
-        expectedResult =! OgimetObservationsProvider.fetchObservationsByInterval 
-            synopParser 
-            httpGetFunc 
-            (StationNumber stationNumber.Get, interval)
+    [<Property>]
+    member this. ``FetchObservationsByInterval returns empty list for non SYNOP input string``
+            (date : DateTime)
+            (stationNumber : PositiveInt)
+            interval
+            (synopStr : SingleLineString) =
+
+        let roundedDate = DateTime.roundToHours date
+        let observationString = 
+            sprintf "%05d,%04d,%02d,%02d,%02d,%02d,AAXY %02d%02d1 %s" 
+                stationNumber.Get date.Year date.Month date.Day date.Hour date.Minute 
+                roundedDate.Day roundedDate.Hour synopStr.Get
+
+        let synopParser _ =
+            raise (Exception("SYNOP parser should not be called"))
+        let httpGetFunc _ _ = 
+            (HttpStatusCode.OK, observationString)
+
+        let result = 
+            OgimetObservationsProvider.fetchObservationsByInterval 
+                synopParser httpGetFunc (StationNumber stationNumber.Get, interval)        
+        result =! []
