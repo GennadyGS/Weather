a4b4ebed8d3c76fb59d7172d00632a7c26ef3741
Gennadii Saltyshchak <GennadyGS@gmail.com>
2017-05-02 12:35:22 +0300

    Move more parsing responsibilities to OgimetObservationsProvider

--- Weather.DataProvider/OgimetObservationsProvider.fs
+++ Weather.DataProvider/OgimetObservationsProvider.fs
@@ -31,27 +31,29 @@ let private getUrlQueryParams (StationNumber stationNumber) dateFrom dateTo =
     
 let private tryParseHeader string =
     match string with
-    | Regex @"^(\d{5}),(\d{4}),(\d{2}),(\d{2}),(\d{2}),(\d{2}),AAXX(.*)$" 
-        [Int(stationNumber); Int(year); Int(month); Int(day); Int(hour); Int(minute); synopString] -> 
+    | Regex @"^(\d{5}),(\d{4}),(\d{2}),(\d{2}),(\d{2}),(\d{2}),AAXX (\d{2})(\d{2})\d (.*)$" 
+        [Int(stationNumber); Int(year); Int(month); Int(day); Int(hour); Int(minute); 
+         Int(registerDay); Int(registerHour); synopString] -> 
             let roundedObservationTime = DateTime(year, month, day, hour, minute, 0) |> DateTime.roundToHours
-            let header =
-                { StationNumber = StationNumber stationNumber
-                  ObservationTime = 
-                    { Date = roundedObservationTime.Date
-                      Hour = byte roundedObservationTime.Hour }}
-            Some <| Success (header, synopString)
+            if registerDay <> roundedObservationTime.Day then
+                Some (Failure <| InvalidObservationHeaderFormat (sprintf "Registration day does not match the header: '%s'" string))
+            elif registerHour <> int roundedObservationTime.Hour then
+                Some (Failure <| InvalidObservationHeaderFormat (sprintf "Registration hour does not match the header: '%s'" string))
+            else 
+                let header =
+                    { StationNumber = StationNumber stationNumber
+                      ObservationTime = 
+                        { Date = roundedObservationTime.Date
+                          Hour = byte roundedObservationTime.Hour }}
+                Some <| Success (header, synopString)
     | Regex @"^(\d{5}),(\d{4}),(\d{2}),(\d{2}),(\d{2}),(\d{2}),[A-Z]{4}(.*)$" _ ->
         None
     | _ -> Some (Failure (InvalidObservationHeaderFormat string))
 
-let private safeToObservation (header : ObservationHeader) synop synopStr =
+let private safeCreateObservation (header : ObservationHeader) synop synopStr =
     let (StationNumber headerStationNumber) = header.StationNumber
     if synop.StationNumber <> headerStationNumber then
         Failure <| InvalidObservationFormat (header, sprintf "Expected station number %d in SYNOP: '%s'" headerStationNumber synopStr)
-    elif synop.Day <> byte header.ObservationTime.Date.Day then
-        Failure <| InvalidObservationFormat (header, sprintf "Expected day %d in SYNOP: '%s'" header.ObservationTime.Date.Day synopStr)
-    elif Math.Abs(int synop.Hour - int header.ObservationTime.Hour) > 1 then
-        Failure <| InvalidObservationFormat (header, sprintf "Expected hour %d in SYNOP: '%s'" header.ObservationTime.Hour synopStr)
     else
         Success { Header = header
                   Temperature = synop.Temperature }
@@ -60,7 +62,7 @@ let private parseSynop parser (header, synopStr) =
     synopStr
     |> parser
     |> Result.bindBoth
-        (fun synop -> safeToObservation header synop synopStr)
+        (fun synop -> safeCreateObservation header synop synopStr)
         (fun message -> Failure <| InvalidObservationFormat (header, message))
 
 let private tryParseObservation synopParser = 
