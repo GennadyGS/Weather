c880c079b6c6d04e343a00e7c4191c93a7fe8330
Gennadii Saltyshchak <GennadyGS@gmail.com>
2017-05-09 10:22:39 +0300

    Remove unused functions from Database modules

--- Weather.Logic/Database.fs
+++ Weather.Logic/Database.fs
@@ -22,17 +22,9 @@ let inline readDataContext2 (func : 'dc -> 'a -> IQueryable<'b>) =
     fun connectionString a -> 
         readDataContext (fun dataContext -> func dataContext a) connectionString
 
-let inline readDataContext3 (func : 'dc -> 'a -> 'b -> IQueryable<'c>) = 
-    fun connectionString a b -> 
-        readDataContext (fun dataContext -> func dataContext a b) connectionString
-
 let inline writeDataContext (func : 'dc -> unit) = 
     Utils.Database.writeDataContext func >> Result.mapFailure DatabaseError
 
 let inline writeDataContext2 (func : 'dc -> 'a -> unit) = 
     fun connectionString a ->
         writeDataContext (fun dataContext -> func dataContext a) connectionString
-
-let inline writeDataContext3 (func : 'dc -> 'a -> 'b -> unit) = 
-    fun connectionString a b ->
-        writeDataContext (fun dataContext -> func dataContext a b) connectionString
