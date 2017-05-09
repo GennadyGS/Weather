e93a442f786d7c569a9b186a9219dba42f1d62fc
Gennadii Saltyshchak <GennadyGS@gmail.com>
2017-05-09 11:09:57 +0300

    Remove unused functions from Weather.Logic/Database.fs

--- Weather.Logic/Database.fs
+++ Weather.Logic/Database.fs
@@ -5,16 +5,6 @@ open Weather
 open Weather.Utils
 open Weather.Model
 
-let runQuerySafe query = 
-    query
-    |> Utils.Database.runQuerySafe
-    |> Result.mapFailure DatabaseError
-
-let inline saveChangesSafe dataContext = 
-    dataContext
-    |> Utils.Database.saveChangesSafe
-    |> Result.mapFailure DatabaseError
-
 let inline readDataContext (func : 'dc -> 'a -> IQueryable<'b>) = 
     fun connectionString a ->
         Utils.Database.readDataContext func connectionString a 
